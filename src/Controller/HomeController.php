555335b23128bc8b7662d06bc728a63fac169f3d
dairo <dairosantab@gmail.com>
2026-07-05 18:47:34 -0500

    fix(add all category)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Categoria;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    /** @var EntityManagerInterface */
+    private $em;    
+
+    public function __construct(EntityManagerInterface $em) {
+        $this->em = $em;
+    }
+
     public function index(): Response
     {
+        $allCategory = $this->em->getRepository(Categoria::class)->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'allCategory' => $allCategory,
         ]);
     }
 
